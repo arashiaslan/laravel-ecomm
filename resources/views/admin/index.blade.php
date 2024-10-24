ce7a2893764dd8c3e7d725363f00279e6647fe18
arashiaslan <imaqil.mj@gmail.com>
2024-10-24 11:46:27 +0700

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1> Admin Ecomm Dashboard</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\AdminController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+Route::middleware(['auth', AdminMiddleware::class])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
