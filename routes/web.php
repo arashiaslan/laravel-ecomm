a5b7512b35fe85d8faf8b161667cf48ad2340e8b
arashiaslan <imaqil.mj@gmail.com>
2024-10-27 21:13:34 +0700

    lupa comit kmrn

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,17 @@ Route::middleware(['auth'])->group(function(){
 });
 Route::middleware(['auth', AdminMiddleware::class])->group(function(){
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
+    Route::get('/admin/brands/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
+    Route::post('/admin/brands/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+    Route::get('/admin/brands/edit/{id}', [AdminController::class, 'brand_edit'])->name('admin.brand.edit');
+    Route::put('/admin/brands/update', [AdminController::class, 'brand_update'])->name('admin.brand.update');
+    Route::delete('/admin/brands/{id}/delete', [AdminController::class, 'brand_delete'])->name('admin.brand.delete');
+
+    Route::get('/admin/categories', [AdminController::class, 'categories'])->name('admin.categories');
+    Route::get('/admin/categories/add', [AdminController::class, 'add_category'])->name('admin.category.add');
+    Route::post('/admin/categories/store', [AdminController::class, 'category_store'])->name('admin.category.store');
+    Route::get('/admin/categories/edit/{id}', [AdminController::class, 'category_edit'])->name('admin.category.edit');
+    Route::put('/admin/categories/update', [AdminController::class, 'category_update'])->name('admin.category.update');
+    Route::delete('/admin/categories/{id}/delete', [AdminController::class, 'category_delete'])->name('admin.category.delete');
 });
